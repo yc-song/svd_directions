ca678b2e207d2edab32a699ae6263a68d6e841de
Jonghyun (Jong) Song <57248081+yc-song@users.noreply.github.com>
2025-04-19 22:53:21 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@ pip install -r requirements.txt --user
 
 # setup pysvelte for visualization:
 # we use this fork from tomfrederik to enable pip installation:
-git clone https://github.com/TomFrederik/PySvelte.git
+# git clone https://github.com/TomFrederik/PySvelte.git
 
 # copy our visualization code from the repo to pysvelte:
-cp -r visualization/* PySvelte/src/
+# cp -r visualization/* PySvelte/src/
 
 cd PySvelte
 pip install -e .
